6d03ea5b44b5cb745d23f9b4aa3c40bc5183111b
Shirin <g00363458@gmit.ie>
2019-02-13 15:56:17 +0000

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  
 })
 export class AppComponent {
   title = 'data-binding-app';
+  counter:number = 0;
+  counter1:number =0;
+
+  hide:boolean = true;
+  onButtonClick(){
+    console.log("Button clicked");
+    this.counter++;
+    }
+    onDblClick(){
+      console.log("Image double clicked");
+      if(this.hide == true)
+      {
+        this.hide = false;
+       }    
+       else{
+         this.hide = true;
+       }  
+     // this.counter1++;
+    }
 }
+
